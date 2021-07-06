5a3f4017d043c60a90568687ac0a0c9cda3cb4ce
Md. Abedur Rahman <abed.baust@gmail.com>
2021-07-06 23:17:11 +0600

    d

---
+++ app/src/main/java/com/abed/e_comkotlin/createAccountInformation.kt
@@ -0,0 +1,11 @@
+package com.abed.e_comkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createAccountInformation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account_information)
+    }
+}
--- app/src/main/java/com/abed/e_comkotlin/createAccountScreen.kt
+++ app/src/main/java/com/abed/e_comkotlin/createAccountScreen.kt
@@ -1,11 +1,23 @@
 package com.abed.e_comkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 
 class createAccountScreen : AppCompatActivity() {
+    lateinit var view: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account_screen)
+
+        view = findViewById(R.id.joinbtn);
+        view.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, createAccountInformation::class.java)
+            startActivity(intent)
+        })
+
+
     }
 }
