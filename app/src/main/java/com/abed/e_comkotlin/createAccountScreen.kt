c3da19742629c4ca489e0df5b8863c9c17918493
Md. Abedur Rahman <abed.baust@gmail.com>
2021-07-06 22:31:55 +0600

    Design 2nd layout

---
+++ app/src/main/java/com/abed/e_comkotlin/createAccountScreen.kt
@@ -0,0 +1,11 @@
+package com.abed.e_comkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createAccountScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account_screen)
+    }
+}
---
+++ app/src/main/java/com/abed/e_comkotlin/splashScreen.kt
@@ -0,0 +1,23 @@
+package com.abed.e_comkotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splashScreen : AppCompatActivity() {
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, createAccountScreen::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
+
